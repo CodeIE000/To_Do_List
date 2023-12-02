7fb559e621a98b96d02519a50b8be78ccc9a09b3
Code <idulsa.emmannicholasblabe.000@gmail.com>
2023-12-02 21:34:25 +0800

    asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdadsasdasdasdasdasdasdasdasdasdasd asdasd

---
+++ lib/pages/introduction.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:todolist_app/pages/home.dart';
+import 'package:todolist_app/theme/colors.dart';
+
+class Introduction extends StatefulWidget {
+  void Function()? onTap;
+  Introduction({super.key});
+
+  @override
+  State<Introduction> createState() => _IntroductionState();
+}
+
+class _IntroductionState extends State<Introduction> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "To-Do List",
+            style: TextStyle(
+                color: Colors.white, fontSize: 25, fontWeight: FontWeight.w900),
+          ),
+          const SizedBox(height: 20),
+          GestureDetector(
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Home(),
+              ),
+            ),
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              decoration: BoxDecoration(
+                  color: objectGreen, borderRadius: BorderRadius.circular(20)),
+              child: Text(
+                "Be Productive!",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
---
+++ lib/theme/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+var backgroundColor = Color.fromARGB(255, 8, 8, 34);
+var objectGreen = Color.fromARGB(255, 1, 200, 104);
